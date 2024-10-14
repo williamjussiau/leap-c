a21ca0732c7d2f9f5d86c637c508c09a8fc37e7c
Confectio <leonard.fichtner@gmail.com>
2024-10-14 13:53:36 +0200

    Implement autograd MPC solution module

--- seal/nn/autograd.py
+++ seal/nn/autograd.py
@@ -8,6 +8,8 @@ import torch.autograd as autograd
 import torch.nn as nn
 import casadi as ca
 from acados_template import AcadosOcp, AcadosOcpSolver, AcadosSimSolver
+from seal.util import tensor_to_numpy
+from seal.mpc import MPC
 
 
 class AutogradCasadiFunction(autograd.Function):
@@ -44,7 +46,7 @@ class AutogradCasadiFunction(autograd.Function):
 
         # compute the jacobian
         grad_input = ctx.jtimes(inputs, grad_output).T
-        # split the grads for the individual inputs 
+        # split the grads for the individual inputs
         grad_input = np.split(grad_input, splits, axis=1)
         # convert to tensor
         grad_input = [torch.tensor(grad, device=device, dtype=dtype) for grad in grad_input]
@@ -60,7 +62,6 @@ class DynamicsSimFunction(autograd.Function):
         batch_size = x.shape[0]
         xdim = x.shape[1]
         udim = u.shape[1]
-
 
         # assert that the inputs are 1D and batched assuming first dimension is batch size
         x = x.detach().cpu().numpy()
@@ -107,3 +108,59 @@ class DynamicsSimFunction(autograd.Function):
         return (None, grad_x, grad_u, None)
 
 
+class MPCSolutionFunction(autograd.Function):
+    @staticmethod
+    def forward(ctx, mpc: MPC, x0: torch.Tensor, p: torch.Tensor,
+                initializations: list[dict[str, np.ndarray]] | None) -> torch.Tensor:
+        assert initializations is None or "p" not in initializations.keys(), "p should be passed explicitly as argument here"
+        device = x0.device
+        dtype = x0.dtype
+        batch_size = x0.shape[0]
+        assert initializations is None or len(initializations) == batch_size
+        xdim = x0.shape[1]
+        pdim = p.shape[1]
+        udim = mpc.ocp.dims.nu
+
+        x0 = tensor_to_numpy(x0)
+        p = tensor_to_numpy(p)
+        if initializations is not None:
+            for i, sample_init in initializations:
+                sample_init["p"] = p[i, :]
+        else:
+            initializations = [{"p": p[i, :]} for i in range(batch_size)]
+
+        u = np.zeros((batch_size, udim))
+        dudp = np.zeros((batch_size, udim, pdim))
+        dudx = np.zeros((batch_size, udim, xdim))
+        status = np.zeros(batch_size, dtype=np.int8)
+
+        for i in range(batch_size):
+            u[i, :], dudp[i, :, :], status[i], dudx[i, :, :] = mpc.pi_update(
+                x0=x0[i, :], initialization=initializations[i], return_dudx=True)
+
+        ctx.dudp = dudp
+        ctx.dudx = dudx
+
+        u = torch.tensor(u, device=device, dtype=dtype)
+        status = torch.tensor(status, device=device, dtype=torch.int8)
+
+        ctx.mark_non_differentiable(status)
+
+        return u, status
+
+    @staticmethod
+    @autograd.function.once_differentiable
+    def backward(ctx, *grad_outputs):
+
+        dLdu, _ = grad_outputs
+
+        device = dLdu.device
+        dtype = dLdu.dtype
+
+        dudx = torch.tensor(ctx.dudx, device=device, dtype=dtype)
+        dudp = torch.tensor(ctx.dudp, device=device, dtype=dtype)
+
+        grad_x = torch.einsum("bkj,bk->bj", dudx, dLdu)
+        grad_p = torch.einsum("bkj,bk->bj", dudp, dLdu)
+
+        return (None, grad_x, grad_p, None, None)
--- seal/util.py
+++ seal/util.py
@@ -1,4 +1,6 @@
+import torch
 import casadi as ca
+import numpy as np
 
 
 def find_idx_for_labels(sub_vars: ca.SX, sub_label: str) -> list[int]:
@@ -8,3 +10,7 @@ def find_idx_for_labels(sub_vars: ca.SX, sub_label: str) -> list[int]:
         for idx, label in enumerate(sub_vars.str().strip("[]").split(", "))
         if sub_label in label
     ]
+
+
+def tensor_to_numpy(tensor: torch.Tensor):
+    return tensor.detach().cpu().numpy()

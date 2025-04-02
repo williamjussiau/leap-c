beb2ad27ab81b739fd0005d25f63c617e739ce86
dirkpr <31849867+dirkpr@users.noreply.github.com>
2025-04-02 13:43:14 +0200

    Add documentation workflow (#55)
    Workflow to build `docs` and host it on
    [github](https://leap-c.github.io/leap-c/)
    
    ---------
    
    Co-authored-by: Josip Kir Hromatko <36133788+josipkh@users.noreply.github.com>

--- install_new_casadi_py311_arm.sh
+++
@@ -1,3 +0,0 @@
-curl -L --remote-name https://github.com/casadi/casadi/releases/download/nightly-issue_4013/casadi-3.6.7.dev+issue.4013-cp311-none-macosx_11_0_arm64.whl
-pip install casadi-3.6.7.dev+issue.4013-cp311-none-macosx_11_0_arm64.whl
-rm casadi-3.6.7.dev+issue.4013-cp311-none-macosx_11_0_arm64.whl
--- install_new_casadi_py311_x86_64.sh
+++
@@ -1,3 +0,0 @@
-curl -L --remote-name https://github.com/casadi/casadi/releases/download/nightly-main/casadi-3.6.7.dev+main-cp311-none-manylinux2014_x86_64.whl;
-pip install casadi-3.6.7.dev+main-cp311-none-manylinux2014_x86_64.whl;
-rm casadi-3.6.7.dev+main-cp311-none-manylinux2014_x86_64.whl
